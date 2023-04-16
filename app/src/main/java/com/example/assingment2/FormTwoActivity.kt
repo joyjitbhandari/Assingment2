9d1259531045e47e0a08f9e9c4ea1b81e479e09e
joyjitbhandari <joyjitbhandari91210@gmail.com>
2023-04-16 09:46:17 +0530

    final change

--- app/src/main/java/com/example/assingment2/FormTwoActivity.kt
+++ app/src/main/java/com/example/assingment2/FormTwoActivity.kt
@@ -1,24 +1,88 @@
 package com.example.assingment2
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import android.window.OnBackInvokedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.example.assingment2.databinding.ActivityForm2Binding
+import com.example.assingment2.model.PersonList
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import org.json.JSONArray
+import org.json.JSONObject
 
-class FormTwoActivity : AppCompatActivity() {
+
+class FormTwoActivity : AppCompatActivity() , View.OnClickListener{
     lateinit var binding: ActivityForm2Binding
+
+    lateinit var jsonArray: JSONArray
+    var item = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityForm2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val intent = intent
+        val jsonData = intent.getStringExtra("jsonArray")
+         jsonArray = JSONArray(jsonData)
 
-        val name = intent.getStringExtra("name")
-        val mobile = intent.getStringExtra("mobile")
-        val date = intent.getStringExtra("date")
+        val jsonObject = jsonArray.getJSONObject(item)
+        Log.d("jsonobject", "${jsonArray}")
+        binding.item1Name.text = jsonObject.getString("name")
+        binding.item1Add.text = jsonObject.getString("add")
+        binding.item1Mob.text = jsonObject.getString("mob")
+        binding.item1Date.text = jsonObject.getString("date")
+        binding.item1Loc.text = jsonObject.getString("loc")
+        binding.item1Desc.text = jsonObject.getString("desc")
+        binding.item1Test.text = jsonObject.getString("test")
+    }
+    override fun onClick(view: View?) {
+        when(view){
 
-        binding.textitem1.text = name
-        binding.textitem2.text = mobile
-        binding.textitem3.text = date
+            binding.backBtn->{
+                onBackPressed()
+            }
+            binding.yesBtn->
+            {
+                finishAffinity()
+            }
+            binding.noBtn->{
+                onBackPressed()
+            }
+            binding.paginationRight->{
+                item++
+                if(item<jsonArray.length()){
+                    val jsonObject = jsonArray.getJSONObject(item)
+                    binding.item1Name.text = jsonObject.getString("name")
+                    binding.item1Add.text = jsonObject.getString("add")
+                    binding.item1Mob.text = jsonObject.getString("mob")
+                    binding.item1Date.text = jsonObject.getString("date")
+                    binding.item1Loc.text = jsonObject.getString("loc")
+                    binding.item1Desc.text = jsonObject.getString("desc")
+                    binding.item1Test.text = jsonObject.getString("test")
+                }else{
+                    Toast.makeText(this, "No more data", Toast.LENGTH_SHORT).show()
+                    item=jsonArray.length()-1
+                }
+            }
+            binding.paginationLeft->{
+                item--
+                if(item>=0){
+                    val jsonObject = jsonArray.getJSONObject(item)
+                    binding.item1Name.text = jsonObject.getString("name")
+                    binding.item1Add.text = jsonObject.getString("add")
+                    binding.item1Mob.text = jsonObject.getString("mob")
+                    binding.item1Date.text = jsonObject.getString("date")
+                    binding.item1Loc.text = jsonObject.getString("loc")
+                    binding.item1Desc.text = jsonObject.getString("desc")
+                    binding.item1Test.text = jsonObject.getString("test")
+                }else{
+                    Toast.makeText(this, "No more data", Toast.LENGTH_SHORT).show()
+                    item = 0
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/assingment2/MainActivity.kt
+++ app/src/main/java/com/example/assingment2/MainActivity.kt
@@ -1,71 +1,112 @@
 package com.example.assingment2
-
+import android.app.DatePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.assingment2.databinding.ActivityMainBinding
 import com.example.assingment2.model.PersonList
+import org.json.JSONArray
 import org.json.JSONObject
+import java.util.*
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var jsonObject: JSONObject
     private lateinit var binding: ActivityMainBinding
+    val jsonArray = JSONArray()
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-
+        //for citi drop down view
         val cities = resources.getStringArray(R.array.cities)
         val arrayAdapter = ArrayAdapter(this, R.layout.drop_down_item_list, cities)
         binding.atLocation.setAdapter(arrayAdapter)
+
+
+        // for date picker
+        val dateEdt = binding.EdtDate
+        dateEdt.setOnClickListener {
+            val c = Calendar.getInstance()
+            val year = c[Calendar.YEAR]
+            val month = c[Calendar.MONTH]
+            val day = c[Calendar.DAY_OF_MONTH]
+            val datePickerDialog = DatePickerDialog(
+                this@MainActivity,
+                { view, year, monthOfYear, dayOfMonth ->
+                    dateEdt.setText(dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                },
+                year, month, day
+            )
+            datePickerDialog.show()
+        }
     }
 
     override fun onClick(view: View?) {
         when (view) {
             binding.saveBtn -> {
+                if(count<10) {
                     val person = PersonList(
-                    binding.etName.text.toString(),
-                    binding.etAddress.text.toString(),
-                    binding.etDate.text.toString(),
-                    binding.etMobile.text.toString(),
-                    binding.atLocation.text.toString(),
-                    binding.etDesc.text.toString(),
-                    binding.etTestField.text.toString()
-                )
-                if (person.name.isNotBlank() && person.address.isNotBlank() && person.date.isNotBlank() && person.test.isNotBlank() && person.mobile.isNotBlank()) {
-                    jsonObject = JSONObject()
-                    jsonObject.put("name", person.name)
-                    jsonObject.put("address", person.address)
-                    jsonObject.put("date", person.date)
-                    jsonObject.put("mobile", person.mobile)
-                    jsonObject.put("loc", person.loc)
-                    jsonObject.put("desc", person.desc)
-                    jsonObject.put("test", person.test)
+                        binding.etName.text.toString(),
+                        binding.etAddress.text.toString(),
+                        binding.EdtDate.text.toString(),
+                        binding.etMobile.text.toString(),
+                        binding.atLocation.text.toString(),
+                        binding.etDesc.text.toString(),
+                        binding.etTestField.text.toString()
+                    )
 
+                    if (person.name.isNotBlank() && person.address.isNotBlank() && person.date.isNotBlank() && person.test.isNotBlank() && person.mobile.isNotBlank()) {
+                        val jsonObject = JSONObject()
+                        jsonObject.put("name", person.name)
+                        jsonObject.put("add", person.address)
+                        jsonObject.put("mob", person.mobile)
+                        jsonObject.put("date", person.date)
+                        jsonObject.put("loc", person.loc)
+                        jsonObject.put("desc", person.desc)
+                        jsonObject.put("test", person.test)
+
+                        jsonArray.put(jsonObject)
+
+                        Toast.makeText(
+                            this,
+                            "All data is converted into json value",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+
+                        Log.d("jsonArray", "${jsonArray.toString()}")
+                    } else {
+                        Toast.makeText(this, "Enter all the above credentials", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    count++
+                }else{
                     Toast.makeText(
                         this,
-                        "All data is converted into json value",
+                        "You can't add more than 10 details",
                         Toast.LENGTH_SHORT
                     )
-                        .show()
-                } else {
-                    Toast.makeText(this, "Enter All the above credentials", Toast.LENGTH_SHORT)
                         .show()
                 }
 
             }
 
             binding.nextBtn -> {
+                if(jsonArray.length()>0) {
+                    Log.d("jsonArrayData", "${jsonArray}")
                     val intent = Intent(this, FormTwoActivity::class.java)
-                    intent.putExtra("name",binding.etName.text.toString())
-                    intent.putExtra("mobile",binding.etMobile.text.toString())
-                    intent.putExtra("date",binding.etDate.text.toString())
+                    intent.putExtra("jsonArray", jsonArray.toString())
                     startActivity(intent)
+                }else{
+                    Toast.makeText(this, "Enter all the above credentials", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
